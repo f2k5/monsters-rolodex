4b9d0ba87804e6e32d3429fa34d654f1a83e984a
fardinkhan <fardin_2510@hotmail.com>
2021-01-28 13:50:24 -0800

    adding initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+
+    //console.log(this.state);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  //using arrow functions we automatically bind "this" to the function we are using
+  handleChange = (event) => {
+    this.setState({searchField: event.target.value});
+  }
+
+  //the render method returns any HTML that we want
+  render () {
+
+    //destructing:
+    //destructing helps pull properties off of objects and set them to constants that we put inside curly braces, aka the curly braces contains those objects:
+    const { monsters, searchField } = this.state;
+
+    //the above is the equivalent of saying:
+    // const searchFeild = this.state.searchFeild;
+    // const monsters = this.state.monsters;
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+  
+    return (
+      <div className = "App">
+        <header>
+          <h1> Monsters Rolodex </h1>
+        </header>
+
+        <SearchBox 
+          placeholder = "search monster"
+          handleChange = {this.handleChange}
+        />
+
+        <CardList monsters = {filteredMonsters} />
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./card-list.styles.css";
+import Card from "../card/card.component";
+
+function CardList (props) {
+    // console.log(props.children);
+    // console.log(props);
+    return (
+        <div className = "card-list">
+        {props.monsters.map(monster =>
+            <Card key = {monster.id} monster = {monster}/>
+        )}
+        </div>
+    );
+}
+
+export default CardList;
